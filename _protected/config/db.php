1c4de6bb1ff5ebda562dc994e833f3748ddbf1d6
buqento <buqento@gmail.com>
2018-12-02 11:00:36 +0700

    new

--- _protected/config/db.php
+++ _protected/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
 
-    'dsn' => 'mysql:host=localhost;dbname=klasisambon',
+    'dsn' => 'mysql:host=localhost;dbname=timoerin_klasis',
 
     'username' => 'timoerin_bvqento',
     'password' => 'Manggureb3!',
